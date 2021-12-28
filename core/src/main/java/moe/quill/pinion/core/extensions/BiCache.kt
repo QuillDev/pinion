f9a41298151687958186f631366478cc548791e8
Robert Brunson <siinspam@gmail.com>
2021-12-28 10:57:22 -0500

    added the bi-map

---
+++ core/src/main/java/moe/quill/pinion/core/extensions/BiCache.kt
@@ -0,0 +1,58 @@
+package moe.quill.pinion.core.extensions
+
+/**
+ * Create a Bi-directional cache backed by hashmaps,
+ * this should **ONLY** be used when **BOTH** keys and values
+ * will be unique in the map.
+ */
+class BiCache<T, Z>(vararg initial: Pair<T, Z>) {
+
+    private val leftCache = mutableMapOf<T, Z>()
+    private val rightCache = mutableMapOf<Z, T>()
+
+    init {
+        initial.forEach { (left, right) ->
+            leftCache[left] = right
+            rightCache[right] = left
+        }
+    }
+
+    @JvmName("setLeft")
+    operator fun set(key: T, value: Z) {
+        leftCache[key] = value
+        rightCache[value] = key
+    }
+
+    @JvmName("setRight")
+    operator fun set(key: Z, value: T) {
+        set(value, key)
+    }
+
+    @JvmName("getLeft")
+    operator fun get(key: T): Z? {
+        return leftCache[key]
+    }
+
+    @JvmName("getRight")
+    operator fun get(key: Z): T? {
+        return rightCache[key]
+    }
+
+    @JvmName("minusAssignLeft")
+    operator fun minusAssign(key: T) {
+        val value = leftCache[key] ?: return
+        leftCache -= key
+        rightCache -= value
+    }
+
+    @JvmName("minusAssignRight")
+    operator fun minusAssign(key: Z) {
+        val value = rightCache[key] ?: return
+        rightCache -= key
+        leftCache -= value
+    }
+}
+
+fun <T, Z> biCache(vararg initial: Pair<T, Z>) = BiCache(*initial)
+
+
---
+++ glow/src/main/java/moe/quill/pinion/glow/GlowData.kt
@@ -0,0 +1,6 @@
+package moe.quill.pinion.glow
+
+import org.bukkit.block.Block
+import org.bukkit.entity.Player
+
+data class GlowData(val block: Block, val viewer: Player)
