dc521ce66f080bb8397028586c4f5a4473397988
Robert Brunson <siinspam@gmail.com>
2021-12-28 19:21:05 -0500

    that makes more sense as a map

--- core/src/main/java/moe/quill/pinion/core/util/EntityUtil.kt
+++ core/src/main/java/moe/quill/pinion/core/util/EntityUtil.kt
@@ -1,16 +1,15 @@
 package moe.quill.pinion.core.util
 
-import moe.quill.pinion.core.extensions.biCache
 import org.bukkit.Location
 import org.bukkit.entity.Entity
 import org.bukkit.entity.EntityType
 import org.bukkit.event.entity.CreatureSpawnEvent
 import kotlin.reflect.KClass
 
-val entityTypeCache = biCache(*EntityType.values().mapNotNull {
+val entityTypeCache = EntityType.values().mapNotNull {
     val klass = it.entityClass ?: return@mapNotNull null
-    return@mapNotNull it to klass
-}.toTypedArray())
+    return@mapNotNull klass to it
+}.toMap()
 
 fun <T : Entity> spawnEntity(klass: KClass<out T>, location: Location, preSpawn: (T) -> Unit = {}): T? {
     val type = entityTypeCache[klass.java] ?: return null
